24e4c0ff38dc5aa88fc6d8b6be37dcebcc23b7e0
Joel Rahman <joel@flowmatters.com.au>
2018-07-27 14:55:34 +1000

    running single models (and ensembles) via the shared library

--- openwater/lib.py
+++ openwater/lib.py
@@ -1,6 +1,10 @@
 import sys
 import ctypes
+import numpy as np
 _the_library = None
+
+def _conv(arr):
+    return [arr.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),*[ctypes.c_int(i) for i in arr.shape]]
 
 def _create_model_func(model_name,description):
   from .discovery import _lib_path, _collect_arguments, _make_model_doc
@@ -12,9 +16,31 @@ def _create_model_func(model_name,description):
   thismodule = sys.modules[__name__]
 
   def model_func(*args,**kwargs):
-    call = [ctypes.create_string_buffer(bytes(model_name,'ascii'))]
+    inputs, params, _, _ = _collect_arguments(description,args,kwargs)
+    if len(inputs) and len(inputs[0].shape)==1:
+      inputs = [i.reshape(1,len(i)) for i in inputs]
+    inputs = np.stack(inputs,axis=1)
 
-    #_the_library.
+    params = np.array(params)
+    if len(params.shape)==1:
+      params = params.reshape(params.shape[0],1)
+
+    outputs = np.zeros((inputs.shape[0],
+                        len(description['Outputs']),
+                        inputs.shape[2]),
+                       dtype='d')
+    states = np.zeros((inputs.shape[0],
+                        len(description['States'])))
+
+    call = [ctypes.create_string_buffer(bytes(model_name,'ascii')),
+            *_conv(inputs),
+            *_conv(params),
+            *_conv(states),
+            *_conv(outputs),
+            ctypes.c_bool(False)]
+
+    _the_library.RunSingleModel(*call)
+    return [outputs[:,i,:] for i in range(len(description['Outputs']))]
   
   model_func.__name__ = model_name
 
